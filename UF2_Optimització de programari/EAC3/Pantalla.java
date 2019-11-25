c14ce03f4338ae3c77cb73e08de0585906e79f80
ES28770377E <jfaneca.ext@ayesa.com>
2019-11-25 08:36:36 +0100

    exercici refactoritzar

--- UF2_Optimització de programari/EAC3/Pantalla.java
+++ UF2_Optimització de programari/EAC3/Pantalla.java
@@ -2,10 +2,6 @@ package Projecte;
 
 public class Pantalla {
 
-	
-	private final double ALT = 27.4;
-	private final double AMPLE = 48.7;
-	private final double POLZADES = 2.54;
 
 	/**
 	 * Escriu les polzades d'unes pantalles
@@ -13,17 +9,22 @@ public class Pantalla {
 	public void escriuPolzades() {
 		System.out.println(
 				"La pantalla normal té " + calcularPolzades(1) + " polzades");
-		
+
 		System.out.println("La pantalla de doble ample i de llarg té "
 				+ calcularPolzades(2) + " polzades");
-		
+
 		System.out.println("La pantalla de triple ample i de llarg té "
 				+ calcularPolzades(3) + " polzades");
 	}
 
 	public double calcularPolzades(int pant) {
-		return Math.round(Math.sqrt(pant * ALT * ALT + pant * AMPLE * AMPLE) / POLZADES);
+		double alt = 27.4;
+		double ample = 48.7;
+		double polzades = 2.54;
+		return Math.round(Math.sqrt(pant * alt * alt + pant * ample * ample) / polzades);
 	}
+
+
 
 	// .....altres mètodes de la classe
 	
